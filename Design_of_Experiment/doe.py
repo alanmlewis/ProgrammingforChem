a8448d55829903029924cfb7b309a5176b86b73c
Alan M. Lewis <alan.m.lewis@york.ac.uk>
2025-10-10 14:09:22 +0100

    Add learning curve plot

--- Design_of_Experiment/doe.py
+++ Design_of_Experiment/doe.py
@@ -13,8 +13,8 @@ optimize = False
 features = pd.read_excel('aap9112_data_file_s1.xlsx')
 
 for j in features.keys():
-	print(j,len(features[j].unique()))
-	
+    print(j,len(features[j].unique()))
+    
 features.replace(to_replace='2a, Boronic Acid',value='Boronic Acid',inplace=True)
 features.replace(to_replace='2b, Boronic Ester',value='Boronic Ester',inplace=True)
 features.replace(to_replace='2c, Trifluoroborate',value='Trifluoroborate',inplace=True)
@@ -37,26 +37,26 @@ features = features[['Reactant_1_Name', 'Reactant_2_Name', 'Ligand_Short_Hand', 
 print(yields.shape)
 
 for j in features.keys():
-	print(j,len(features[j].unique()))
+    print(j,len(features[j].unique()))
 
-	box_data = {}
-	print(','.join(features[j].unique()))
-	for i in features[j].unique():
-		idx = np.argwhere(features[j].values == i)
-		box_data[i] = yields[idx].reshape(-1)
-		print(i,box_data[i].shape,sum(yields[idx].reshape(-1)==0))
-	
-	if show_plots:
-		fig,ax = plt.subplots(1,1)
-		ax.boxplot(box_data.values())
-		ax.set_title(j)
-		ax.set_xticklabels(box_data.keys())
-		plt.show()
+    box_data = {}
+    print(','.join(features[j].unique()))
+    for i in features[j].unique():
+        idx = np.argwhere(features[j].values == i)
+        box_data[i] = yields[idx].reshape(-1)
+        print(i,box_data[i].shape,sum(yields[idx].reshape(-1)==0))
+    
+    if show_plots:
+        fig,ax = plt.subplots(1,1)
+        ax.boxplot(box_data.values())
+        ax.set_title(j)
+        ax.set_xticklabels(box_data.keys())
+        plt.show()
 
 features.rename(columns={'Reactant_1_Name': 'Reactant 1', 'Reactant_2_Name': 'Reactant 2', 'Ligand_Short_Hand': 'Ligand', 'Reagent_1_Short_Hand': 'Base', 'Solvent_1_Short_Hand':'Solvent'},inplace=True)
 
 features.to_csv('features.csv',index=False)
-np.savetxt('yields.csv',yields)
+np.savetxt('yields.csv',yields,header='Yields (%)')
 
 encoder = OneHotEncoder(sparse_output=False)
 
@@ -79,31 +79,53 @@ from sklearn.model_selection import RandomizedSearchCV
 
 # Here for testing purposes, remove before making available to students
 if optimize:
-	param_distributions = {
-		'C': loguniform(0.1, 1000.0),
-		'epsilon': loguniform(0.001, 10.0),
-		'kernel': ['linear', 'poly', 'rbf', 'sigmoid']
-	}
+    param_distributions = {
+        'C': loguniform(0.1, 1000.0),
+        'epsilon': loguniform(0.001, 10.0),
+        'kernel': ['linear', 'poly', 'rbf', 'sigmoid']
+    }
 
-	randomized_search = RandomizedSearchCV(
-			estimator = model,
-			param_distributions = param_distributions,
-			n_iter = 100,
-			random_state = 1,
-			verbose = 2
-		)
+    randomized_search = RandomizedSearchCV(
+            estimator = model,
+            param_distributions = param_distributions,
+            n_iter = 100,
+            random_state = 1,
+            verbose = 2
+        )
 
-	randomized_search.fit(x_train, y_train)
-	print(randomized_search.best_params_)
-	y_predict_test = randomized_search.best_estimator_.predict(x_test)
+    randomized_search.fit(x_train, y_train)
+    print(randomized_search.best_params_)
+    y_predict_test = randomized_search.best_estimator_.predict(x_test)
 
 else:
-	# Fit the model
-	model.fit(x_train, y_train)
-	y_predict_test = model.predict(x_test)
+    # Fit the model
+    model.fit(x_train, y_train)
+    y_predict_test = model.predict(x_test)
 
 # Test the accuracy of the model against the test set
 
 accuracy = root_mean_squared_error(y_test, y_predict_test)
 print(f'The root mean square error was {(accuracy)}%!')
 
+errors = []
+
+train_vals = [100,200,500,1000,2000]
+for n_train in [100,200,500,1000,2000]:
+    model.fit(x_train[:n_train], y_train[:n_train])
+    y_predict_test = model.predict(x_test)
+    errors.append(root_mean_squared_error(y_test, y_predict_test))
+
+plt.close()
+
+plt.rcParams.update({'font.size': 10})
+plt.rcParams["figure.figsize"] = (4,2.5)
+plt.rcParams["figure.dpi"] = 200
+
+fig,ax = plt.subplots()
+ax.plot(train_vals,errors,marker='o',ms=6,linewidth=3)
+ax.set_xscale('log')
+ax.set_xlabel('Training Set Size')
+ax.set_ylabel('RMSE / %')
+
+plt.tight_layout()
+plt.savefig('lc.png')
