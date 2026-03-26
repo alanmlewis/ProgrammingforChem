35cf2ec5b7c0830397b3816ff94cc2ebe9a1cfbc
Alan M. Lewis <alan.m.lewis@york.ac.uk>
2026-03-26 20:27:11 +0000

    Proper solution to suffix problem - fitting function name is added followed by a suffix, if given. Default suffix is blank

--- plot.py
+++ plot.py
@@ -39,10 +39,9 @@ if fit == False:
     output = False
 
 # Add a unique suffix to the name of each output file
-# If you are fitting the data, this is the name of the fitting function by default
-# Uncomment the line below to override this
+# If you are fitting the data, this is added after the name of the fitting function
 
-# suffix = "_suffix"
+suffix = ""
 
 # ------------------------------------------------------------------------------------------
 
@@ -58,7 +57,7 @@ try:
 	func = eval(func_name)
 	# Since you've provided an acceptable fitting function, we assume you want to fit
 	fit = True
-	# suffix = '_'+func_name
+	suffix = '_' + func_name + suffix
 	# Since you've provided an acceptable fitting function, we assume you want to output fitting parameters
 	output = True
 	print(f'A best fit to the data will be calculated using the function \"{func_name}\"')
@@ -68,9 +67,7 @@ except:
 	    print("Since you've requested a fit (fit = True), defaulting to a linear line of best fit.\n")
 	    func_name = "linear"
 	    func = eval(func_name)
-	    # suffix = '_'+func_name
-	else:
-	    pass
+	    suffix = '_' + func_name + suffix
 
 # ------------------------------------------------------------------------------------------
 
