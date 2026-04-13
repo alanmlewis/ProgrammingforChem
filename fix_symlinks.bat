2390c15345e5a0fa70088392340f4f45f2b5997c
Alan M. Lewis <alan.m.lewis@york.ac.uk>
2026-04-13 16:04:45 +0100

    Add home directory test.py

--- fix_symlinks.bat
+++ fix_symlinks.bat
@@ -1,9 +1,9 @@
 @echo off
 echo Deleting symlinks
-rm Y1_Kinetics/plot.py
-rm Y1_Kinetics/test.py
-rm Y2_Heat_Capacities/plot.py
-rm Y2_Heat_Capacities/test.py
+del Y1_Kinetics/plot.py
+del Y1_Kinetics/test.py
+del Y2_Heat_Capacities/plot.py
+del Y2_Heat_Capacities/test.py
 echo Configuring Git for Symlinks...
 git config core.symlinks true
 echo Re-linking files...
